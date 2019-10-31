5ee6eac8173f383f191d78c341fb6b3a449c5546
Ayush Mishra <43182109+ayush1208@users.noreply.github.com>
2019-10-31 23:15:04 +0530

    Add files via upload

--- DSA/Programs/quicksort.c
+++ DSA/Programs/quicksort.c
@@ -1,46 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
-int partition(int arr[], int l, int r){
-	int pivot = arr[l];
-	int i = l + 1, j = r;
-	while(i <= j){
-		while(i <= j && arr[i] <= pivot)
-			i++;
-		while(i <= j && arr[j] > pivot)
-			j--;
-		if(i <= j){
-			int temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] = temp;
-		}	
-	}
-	int temp = arr[l];
-	arr[l] = arr[j];
-	arr[j] = temp;
-	return j;	
-}	
-void quicksort(int arr[], int l, int r){
-	if(l < r){
-		int p = l + rand() % (r - l + 1);
-		int temp = arr[l];
-		arr[l] = arr[p];
-		arr[p] = temp;
-		int k = partition(arr, l, r);
-		quicksort(arr, l, k - 1);
-		quicksort(arr, k + 1, r);
-	} 
+void swap(int *a,int *b)
+{
+	int c=*a;
+	*a=*b;
+	*b=c;
 }
-int main(){
-	int arr[] = {10, 50, 30, 90, 80, 20, 40, 70, 60};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printf("Before sorting, array is...\n");
-	for(int i = 0; i < n; i++)
-		printf("%d\t", arr[i]);
-	printf("\n");	
-	quicksort(arr, 0, n - 1);	
-	printf("After sorting, array is...\n");	
-	for(int i = 0; i < n; i++)
-		printf("%d\t", arr[i]);
-	printf("\n");
-	return 0;
+int partition(int *a,int i,int j)
+{
+	int p=a[i];
+	int l=i+1;
+	int r=j;
+	while(l<=r)
+	{
+		while(l<=r && a[l]<=p)
+			l++;
+		while(l<=r && a[r]>p)
+			r--;
+		if(l<=r)
+		{
+			swap(&a[l],&a[r]);
+			l++,r--;
+		}
+	}	
+	swap(&a[r],&a[i]);
+	return r;
+}
+void quicksort(int *a,int i,int j)
+{
+	if(i<j)
+	{
+		int k=partition(a,i,j);
+		quicksort(a,i,k-1);
+		quicksort(a,k+1,j);
+	}
 }
