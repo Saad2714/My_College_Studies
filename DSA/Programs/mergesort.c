181eb5ba60d7f96228668188b819ab870ec4d335
Ayush Mishra <43182109+ayush1208@users.noreply.github.com>
2019-10-31 23:17:57 +0530

    Add files via upload

--- DSA/Programs/mergesort.c
+++ DSA/Programs/mergesort.c
@@ -1,37 +1,34 @@
 #include<stdio.h>
-void merge(int arr[], int l, int m, int r){
-	int n = r - l + 1, temp_arr[n], i = l, j = m + 1, k = 0;
-	while(i <= m && j <= r)
-		if(arr[i] < arr[j])
-			temp_arr[k++] = arr[i++];
-		else 
-			temp_arr[k++] = arr[j++];
-	while(i <= m)
-		temp_arr[k++] = arr[i++];
-	while(j <= r)
-		temp_arr[k++] = arr[j++];	
-	for(int i = 0; i < n; i++)
-		arr[l + i] = temp_arr[i];		
-}
-void mergesort(int arr[], int l, int r){
-	if (l < r){
-		int m = (l + r) / 2;
-		mergesort(arr, l, m);
-		mergesort(arr, m + 1, r);
-		merge(arr, l, m, r);
+#include"mergesort.h"
+void mergesort(int *a,int i,int j)
+{
+	if(i<j)
+	{
+		int m=(i+j)/2;
+		mergesort(a,i,m);
+		mergesort(a,m+1,j);
+		merge(a,i,j,m);
 	}
 }
-int main(){
-	int arr[] = {90, 80, 70, 60, 50, 40, 30, 20, 10};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printf("Before sorting, array is...\n");
-	for(int i = 0; i < n; i++)
-		printf("%d\t", arr[i]);
-	printf("\n");	
-	mergesort(arr, 0, n - 1);	
-	printf("After sorting, array is...\n");	
-	for(int i = 0; i < n; i++)
-		printf("%d\t", arr[i]);
-	printf("\n");
-	return 0;
+void merge(int *a,int i,int j,int m)
+{
+	int l=i;
+	int r=m+1;
+	int p=0;
+	int b[j];
+	while(l<=m && r<=j)
+	{
+		if(a[l]<a[r])
+			b[p++]=a[l++];
+		else 
+			b[p++]=a[r++];
+	}
+	while(l<=m)
+		b[p++]=a[l++];
+	while(r<=j)
+		b[p++]=a[r++];
+	l=i;
+	p=0;
+	while(l<=j)
+		a[l++]=b[p++];
 }
